73eeb5748c2957302d24ef3df953f8704a7b4506
github username <github email address>
2018-11-24 18:15:26 -0500

    Done.

---
+++ app/controllers/school_classes_controller.rb
@@ -0,0 +1,40 @@
+class SchoolClassesController < ApplicationController
+
+  def index
+    @classes = SchoolClass.all
+  end
+
+  def show
+    @class = SchoolClass.find(params[:id])
+  end
+
+  def new
+    @class = SchoolClass.new
+  end
+
+  def create
+    @class = SchoolClass.new(post_params(:title, :room_number))
+    @class.save
+
+    redirect_to school_class_path(@class)
+  end
+
+  def edit
+    @class = SchoolClass.find(params[:id])
+  end
+
+  def update
+    @class = SchoolClass.find(params[:id])
+    @class.update(params.require(:school_class))
+
+    redirect_to school_class_path(@class)
+
+  end
+
+  private
+
+  def post_params(*args)
+    params.require(:school_class).permit(*args)
+  end
+
+end
