7f59b71d812382bed433fbe10b7b23569246ca2a
leannemcabey <leanne.m.cabey@gmail.com>
2018-11-25 18:14:15 -0500

    complete

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,36 @@
+class StudentsController < ApplicationController
+
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = find_student(params)
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    @student = Student.create(params.require(:student).permit(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  def edit
+    @student = find_student(params)
+  end
+
+  def update
+    @student = find_student(params)
+    @student.update(params.require(:student).permit(:first_name, :last_name))
+    redirect_to student_path(@student)
+  end
+
+  private
+
+    def find_student(params)
+      @student = Student.find(params[:id])
+    end
+
+end
